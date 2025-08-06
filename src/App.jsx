4cd0f5cad9d52e8417cbd64820f9f0a1b302819c
Hammad-Hassan1413 <hammad14130@gmail.com>
2025-08-06 18:51:45 +0500

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import NavBar from "./NavBar.jsx";
 import Hero from "./Hero.jsx";
 import Client from "./Clients.jsx";
+import Cards from "./cards.jsx";
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
       <Client />
+      <Cards />
     </>
   );
 }
---
+++ src/Cards.jsx
@@ -0,0 +1,55 @@
+import person from "./assets/person.png";
+import building from "./assets/building.png";
+import group from "./assets/group.png";
+
+function Cards() {
+  const Card = ({ heading, text, image }) => (
+    <div className="card">
+      <img src={image} alt={heading} />
+      <h2>{heading}</h2>
+      <p>{text}</p>
+    </div>
+  );
+  const cardData = [
+    {
+      heading: "Membership Organizations",
+      text: "Our membership management software provides full automation of membership renewals and payments.",
+      image: person,
+    },
+    {
+      heading: "National Associations",
+      text: "Our national associations software helps you manage memberships, events, and payments.",
+      image: building,
+    },
+    {
+      heading: "Clubs & Groups",
+      text: "Easily manage club activities, members, and communication using our tools.",
+      image: group,
+    },
+  ];
+
+  return (
+    <section className="cards">
+      <div className="container">
+        <div className="cards-heading">
+          <h2>
+            Manage your entire community
+            <br /> in a single system
+          </h2>
+          <p>Who is Nextcent suitable for?</p>
+        </div>
+        <div className="cards-flex">
+          {cardData.map((card, index) => (
+            <Card
+              key={index}
+              heading={card.heading}
+              text={card.text}
+              image={card.image}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Cards;
